d449c40fbe804ff0363ca64f03d20444174714ee
andresdev <andresmestra11a@gmail.com>
2021-12-21 18:30:52 -0500

    ajustando a la actulización del curso

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:componentes/src/pages/home_temp.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,9 +8,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Componentes App',
-      debugShowCheckedModeBanner: false,
-      home: HomePageTemp(),
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
     );
   }
 }
