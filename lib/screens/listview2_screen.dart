67dc23230554db5d536760e7673467c2a0bd1144
andresdev <andresmestra11a@gmail.com>
2021-12-21 19:11:20 -0500

    feat: ListView.separated para construir lista con separador

---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Listview2Screen extends StatelessWidget {
+  final options = const ['Megaman', 'Metal Gear', 'Super Manrio'];
+
+  const Listview2Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Listview Tipo 1'),
+      ),
+      body: ListView.separated(
+        itemCount: options.length,
+        itemBuilder: (context, index) => ListTile(
+          title: Text(options[index]),
+          trailing: const Icon(Icons.arrow_forward_ios_outlined),
+        ),
+        separatorBuilder: (_, __) => const Divider(),
+      ),
+    );
+  }
+}
