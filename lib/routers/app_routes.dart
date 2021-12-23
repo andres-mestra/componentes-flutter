a120c8cd58aae25ec356880ec3f04e698e2b11a4
andresdev <andresmestra11a@gmail.com>
2021-12-22 21:20:16 -0500

    feat: modelo para definicion de rutas

--- lib/routers/app_routes.dart
+++ lib/routers/app_routes.dart
@@ -1,8 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:componentes/models/models.dart';
 import 'package:componentes/screens/screens.dart';
 
 class AppRoutes {
   static const initialRoute = 'home';
+
+  static final menuOptions = <MenuOption>[
+    //TODO: Borrar  home
+    MenuOption(
+      route: 'home',
+      name: 'Home Screen',
+      screen: const HomeScreen(),
+      icon: Icons.home,
+    ),
+    MenuOption(
+        route: 'listview1',
+        name: 'Listview tipo 1',
+        screen: const Listview1Screen(),
+        icon: Icons.list_alt),
+    MenuOption(
+        route: 'listview2',
+        name: 'Listview tipo 2',
+        screen: const Listview2Screen(),
+        icon: Icons.list_sharp),
+    MenuOption(
+      route: 'alert',
+      name: 'Alerts',
+      screen: const AlertScreen(),
+      icon: Icons.add_alert_outlined,
+    ),
+    MenuOption(
+      route: 'card',
+      name: 'Cards',
+      screen: const CardScreen(),
+      icon: Icons.card_giftcard_outlined,
+    ),
+  ];
 
   static Map<String, Widget Function(BuildContext)> routes = {
     'home': (BuildContext context) => const HomeScreen(),
