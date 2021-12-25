bd125f4e6a20e7c1dbfaaf0c855cae26086bad14
andresdev <andresmestra11a@gmail.com>
2021-12-25 12:28:56 -0500

    feat: construir un alert y como cerrarlo

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,11 +3,56 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
+  void displayDialog(BuildContext context) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          elevation: 5,
+          title: const Text('Titulo'),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusDirectional.circular(10),
+          ),
+          content: Column(
+            //Se ajusta al tamaño de los hijos
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('Este es el contenido de la alerta'),
+              SizedBox(height: 10),
+              FlutterLogo(size: 100),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancelar'),
+            )
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('AlertScreen'),
+        child: ElevatedButton(
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Text(
+              'Mostrar alerta',
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+          onPressed: () => displayDialog(context),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.close),
+        //Regresa a la ultima pantalla mostrada
+        onPressed: () => Navigator.pop(context),
       ),
     );
   }
