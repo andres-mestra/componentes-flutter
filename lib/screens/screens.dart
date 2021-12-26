96d08504f5a8d6a306d8022cb88ae18ad3b38cf4
andresdev <andresmestra11a@gmail.com>
2021-12-26 13:16:46 -0500

    textformfield, capitalization, autofocus en input

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -3,5 +3,6 @@ export 'package:componentes/screens/animated_screen.dart';
 export 'package:componentes/screens/avatar_screen.dart';
 export 'package:componentes/screens/card_screen.dart';
 export 'package:componentes/screens/home_screen.dart';
+export 'package:componentes/screens/inputs_screen.dart';
 export 'package:componentes/screens/listview1_screen.dart';
 export 'package:componentes/screens/listview2_screen.dart';
