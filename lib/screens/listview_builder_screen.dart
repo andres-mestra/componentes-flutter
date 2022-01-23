e98d1bf505413bd83da9f4b99976c63ae4562680
andresdev <andresmestra11a@gmail.com>
2022-01-23 09:42:35 -0500

    feat: infinite scroll gallery

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
 
-class ListViewBuilderScreen extends StatelessWidget {
+class ListViewBuilderScreen extends StatefulWidget {
   const ListViewBuilderScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ListViewBuilderScreen> createState() => _ListViewBuilderScreenState();
+}
+
+class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
+  final List<int> imagesIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  final ScrollController scrollController = ScrollController();
+
+  //Cuando el state se crea por primera vez
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      //print('${scrollController.position.pixels} , ${scrollController.position.maxScrollExtent}');
+      if ((scrollController.position.pixels + 500) >=
+          scrollController.position.maxScrollExtent) {
+        add5();
+      }
+    });
+  }
+
+  void add5() {
+    final lastId = imagesIds.last;
+    imagesIds.addAll([1, 2, 3, 4, 5].map((e) => lastId + e));
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +39,9 @@ class ListViewBuilderScreen extends StatelessWidget {
         removeTop: true,
         removeBottom: true,
         child: ListView.builder(
-          itemCount: 20,
+          physics: const BouncingScrollPhysics(), //efecto fin lista de ios
+          controller: scrollController,
+          itemCount: imagesIds.length,
           itemBuilder: (context, index) {
             //https://source.unsplash.com/random/300×300
             return FadeInImage(
@@ -21,7 +50,7 @@ class ListViewBuilderScreen extends StatelessWidget {
               fit: BoxFit.cover, // Toma todo el espacio que tiene la imagen
               placeholder: const AssetImage('assets/jar-loading.gif'),
               image: NetworkImage(
-                  'https://source.unsplash.com/500x300/?${index + 1}a'),
+                  'https://source.unsplash.com/500x300/?${imagesIds[index]}a'),
             );
           },
         ),
